c18d7d549b462c307e08423bc0b0829f2678d30b
Carolaine Vieira <54178722+carolaine-vieira@users.noreply.github.com>
2022-01-08 19:56:47 -0300

    swipper config

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,51 @@
-const swipper = () => {
-  const slides = document.querySelectorAll("#recommended .slide");
+const swipper = (container, slidesCount) => {
+  const slides = Array.from(document.querySelectorAll("#recommended .slide"));
   const previousButton = document.querySelector(
     "#recommended .previous-item a"
   );
   const nextButton = document.querySelector("#recommended .next-item a");
   const moveSize = 100 / 6;
+  const maxMove = slides.length - 6 - 1;
   let currentSlide = 0;
+  let draggedIndex;
 
-  previousButton.addEventListener("click", () => {
-    currentSlide > 0 ? (slides[--currentSlide].style.marginLeft = 0) : null;
-  });
+  const handlePreviousItem = () => {
+    currentSlide > 0 ? (slides[--currentSlide].style.marginLeft = "4px") : null;
+  };
 
-  nextButton.addEventListener("click", () => {
-    const maxMove = slides.length - 6 - 1;
+  const handleNextItem = () => {
     currentSlide < maxMove
       ? (slides[currentSlide++].style.marginLeft = `-${moveSize}%`)
       : {};
-  });
+  };
 
-  slides.forEach((slide) => {
-    slide.addEventListener("mouseup", () => {
-      console.log(drag);
+  previousButton.addEventListener("click", handlePreviousItem);
+  nextButton.addEventListener("click", handleNextItem);
+
+  const grabItens = () => {
+    slides.forEach((slide) => {
+      const moveSwipper = (e) => {
+        slide.style.cursor = "grabbing";
+        console.log(e.offsetX);
+
+        if (e.offsetX > 150) {
+        }
+      };
+
+      const grab = (e) => {
+        console.log(e.offsetX);
+        slide.addEventListener("mousemove", moveSwipper);
+      };
+
+      slide.addEventListener("mousedown", grab);
+
+      window.addEventListener("mouseup", () => {
+        slide.style.cursor = "grab";
+        console.log("mouse up");
+
+        slide.removeEventListener("mousemove", moveSwipper);
+      });
     });
-  });
+  };
 };
 swipper();
